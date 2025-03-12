3d961c44d18b4a4cabcaf85639b5acf5b9cd6256
FabioLWT <fabiotamazzia15@gmail.com>
2025-03-12 18:12:54 -0300

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,12 +7,12 @@ const Historico = require("./historico");
 
 const initializeDatabase = async () => {
   try {
-    // Ordem importa devido às chaves estrangeiras
-    await Marcas.createTable(); // Sem dependências
-    await Modelos.createTable(); // Depende de marcas
-    await Usuarios.createTable(); // Sem dependências
-    await Veiculos.createTable(); // Depende de modelos
-    await Historico.createTable(); // Depende de veiculos
+    
+    await Marcas.createTable(); 
+    await Modelos.createTable(); 
+    await Usuarios.createTable(); 
+    await Veiculos.createTable(); 
+    await Historico.createTable(); 
     console.log("Banco de dados inicializado com sucesso!");
   } catch (error) {
     console.error("Erro ao inicializar o banco de dados:", error.stack);
