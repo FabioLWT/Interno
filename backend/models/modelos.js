124326bf5f2040123a48023a7c813123e4765549
FabioLWT <fabiotamazzia15@gmail.com>
2025-03-12 18:13:12 -0300

    Update modelos.js

--- backend/models/modelos.js
+++ backend/models/modelos.js
@@ -12,7 +12,7 @@ const Modelos = {
     await db.query(query);
     console.log("Tabela 'modelos' criada ou já existente.");
 
-    // Inserções de dados de teste
+    
     await db.query(
       "INSERT INTO modelos (nome, marca_id) VALUES ($1, $2) ON CONFLICT DO NOTHING",
       ["Corolla", 1]
